ebd82c83a6a55db69ddd097d26cd90a74b8a0cd2
PivIgor <brauer.igor@gmail.com>
2022-01-20 01:04:30 +0300

    Test coverage uiAutomator

---
+++ app/src/androidTest/java/com/geekbrains/tests/automator/BehaviorTest.kt
@@ -0,0 +1,86 @@
+package com.geekbrains.tests.automator
+
+import android.content.Context
+import android.content.Intent
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SdkSuppress
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.Until
+import com.geekbrains.tests.R
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SdkSuppress(minSdkVersion = 18)
+class BehaviorTest {
+
+    //Класс UiDevice предоставляет доступ к вашему устройству.
+    //Именно через UiDevice вы можете управлять устройством, открывать приложения
+    //и находить нужные элементы на экране
+    private val uiDevice: UiDevice = UiDevice.getInstance(getInstrumentation())
+
+    //Контекст нам понадобится для запуска нужных экранов и получения packageName
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+
+    //Путь к классам нашего приложения, которые мы будем тестировать
+    private val packageName = context.packageName
+
+    @Before
+    fun setUp() {
+        //Для начала сворачиваем все приложения, если у нас что-то запущено
+        uiDevice.pressHome()
+
+        //Запускаем наше приложение
+        val intent = context.packageManager.getLaunchIntentForPackage(packageName)
+
+        //Мы уже проверяли Интент на null в предыдущем тесте, поэтому допускаем, что Интент у нас не null
+        intent!!.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK) //Чистим бэкстек от запущенных ранее Активити
+        context.startActivity(intent)
+
+        //Ждем, когда приложение откроется на смартфоне чтобы начать тестировать его элементы
+        uiDevice.wait(Until.hasObject(By.pkg(packageName).depth(0)), TIMEOUT)
+    }
+
+    @Test
+    fun test_MainActivityIsStarted() {
+        //Через uiDevice находим editText
+        val editText = uiDevice.findObject(By.res(packageName, "searchEditText"))
+        //Проверяем на null
+        assertNotNull(editText)
+    }
+
+    //Убеждаемся, что поиск работает как ожидается
+    @Test
+    fun test_SearchIsPositive() {
+        //Через uiDevice находим editText
+        val editText = uiDevice.findObject(By.res(packageName, "searchEditText"))
+        //Устанавливаем значение
+        editText.text = "UiAutomator"
+        //Отправляем запрос через Espresso
+        onView(withId(R.id.searchEditText))
+            .perform(ViewActions.pressImeActionButton())
+
+        //Ожидаем конкретного события: появления текстового поля totalCountTextView.
+        //Это будет означать, что сервер вернул ответ с какими-то данными, то есть запрос отработал.
+        val changeTest = uiDevice.wait(
+            Until.findObject(By.res(packageName, "totalCountTextView")),
+            TIMEOUT
+        )
+        //Убеждаемся, что сервер вернул корректный результат. Обратите внимание, что количество
+        //результатов может варьироваться во времени, потому что количество репозиториев постоянно меняется.
+        assertTrue(changeTest.text.toString().startsWith("Number of results:"))
+    }
+
+    companion object {
+        private const val TIMEOUT = 5000L
+    }
+}
